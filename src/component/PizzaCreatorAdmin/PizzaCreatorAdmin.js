2080dd30703db1f9cd10494c78a21491878fab37
yuhe3000 <yanliqiang66@gmail.com>
2019-02-26 11:50:47 +1100

    create pizzacreatoradmin project

---
+++ src/component/PizzaCreatorAdmin/PizzaCreatorAdmin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../images/admin.png'
+export default () => (
+  <div>
+    <img src={logo} alt="logo"/>
+    <p>Hello Max</p>
+    </div>
+);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+import PizzaCreatorAdmin from './component/PizzaCreatorAdmin/PizzaCreatorAdmin';
+
+ReactDOM.render(<PizzaCreatorAdmin />, document.querySelector('#app'));
